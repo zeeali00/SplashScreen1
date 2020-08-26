a2d82eaf80db9295000ca60b9571e6b2becedb7e
Zeeshan Ali <zee.ali00@yahoo.com>
2020-08-26 14:53:37 +0500

    SplashScreen

---
+++ Assets/Loading.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Loading : MonoBehaviour
+{
+    public Transform loadingbar;
+    [SerializeField]
+    private float currentAmount;  
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        loadingbar.GetComponent<Image>().fillAmount = currentAmount / 100;
+        currentAmount++;
+    }
+}
